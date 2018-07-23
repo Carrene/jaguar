414aa96d1fbf35745ac7b56da5c711130d50c320
mohamad <khajezade.mohamad@gmail.com>
2018-07-23 16:20:49 +0430

    Fixing test calss name, closes #50

--- jaguar/tests/helpers.py
+++ jaguar/tests/helpers.py
@@ -7,11 +7,7 @@ from restfulpy.testing import ApplicableTestCase
 HERE = path.abspath(path.dirname(__file__))
 
 
-<<<<<<< 6f9f35507b04eb354c398daddcb818b5abd00ca5
 class AutoDocumentationBDDTest(ApplicableTestCase):
-=======
-class BDDTestClass(ApplicableTestCase):
->>>>>>> Creating autodocument for jaguar
 
     @classmethod
     def get_spec_filename(cls, story):
@@ -36,13 +32,8 @@ class BDDTestClass(ApplicableTestCase):
     def given(self, title, dump=True, *args, **kwargs):
         return super().given(
             title = title,
-<<<<<<< 6f9f35507b04eb354c398daddcb818b5abd00ca5
             autodump=dump and self.get_spec_filename,
             autodoc=dump and self.get_markdown_filename,
-=======
-            autodump=self.get_spec_filename if dump else False,
-            autodoc=self.get_markdown_filename if dump else False,
->>>>>>> Creating autodocument for jaguar
             *args,
             **kwargs
         )
