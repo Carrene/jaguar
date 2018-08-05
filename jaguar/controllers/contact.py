dd928204ace6b3edb3d89c3104a34908894ebf60
mkhfring <m.khajezade@carrene.com>
2018-08-05 20:45:25 +0430

    Implementing add to contact, first version

---
+++ jaguar/controllers/contact.py
@@ -0,0 +1,31 @@
+
+from sqlalchemy import and_, or_
+from nanohttp import json, context, validate, HTTPStatus
+from restfulpy.authorization import authorize
+from restfulpy.controllers import ModelRestController
+from restfulpy.orm import DBSession, commit
+
+from jaguar.models import Target, Room, blocked, User, contact
+
+
+class ContactController(ModelRestController):
+    __model__ = User
+
+    @json
+    @User.expose
+    @commit
+    def add(self):
+        source = User.current()
+        user_id = context.form.get('user_id')
+        is_contact = DBSession.query(contact) \
+            .filter(
+                contact.c.source == context.identity.id,
+                contact.c.destination == user_id
+            ).count()
+        if is_contact:
+            raise HTTPStatus('603 Already Added To Contacts')
+        destination = DBSession.query(User) \
+        .filter(User.id == user_id).one_or_none()
+        source.contacts.append(destination)
+        return source
+
