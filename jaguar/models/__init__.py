14e628dd56c3f6082d049425ccf8b51329bd932a
mohamad <khajezade.mohamad@gmail.com>
2018-08-07 12:45:04 +0430

    The contact controller and membership model are changed, closes #18

--- jaguar/models/__init__.py
+++ jaguar/models/__init__.py
@@ -1,5 +1,5 @@
 
 from .target import Target, Room, Direct, target_member
 from .envelop import Envelop
-from .membership import Member, User, blocked, contact
+from .membership import Member, User, blocked, Contact
 from .messaging import ActivationEmail
