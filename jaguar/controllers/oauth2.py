3235960c00ecc4a1f29a7400644e0c97553f6a6b
mohamad <khajezade.mohamad@gmail.com>
2018-09-09 16:57:39 +0430

    Unnecessary inputs are removed

--- jaguar/controllers/oauth2.py
+++ jaguar/controllers/oauth2.py
@@ -3,6 +3,6 @@ from .token import TokenController
 
 class OAUTHController(RestController):
     def __call__(self, *remaining_paths):
-        if len(remaining_paths) and remaining_paths[0] == 'tokens':
-            return TokenController()(*remaining_paths[1:])
+        if len(remaining_paths) ==1  and remaining_paths[0] == 'tokens':
+            return TokenController()()
 
