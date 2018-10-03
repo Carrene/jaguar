82796bc1d5bfc8ec25730689c784b99506e70d54
mohamad <khajezade.mohamad@gmail.com>
2018-10-03 15:33:28 +0330

    The is_deleted field is added to message model, closes #116

--- jaguar/tests/test_delete_messages.py
+++ jaguar/tests/test_delete_messages.py
@@ -8,12 +8,15 @@ class TestDeleteMessage(AutoDocumentationBDDTest):
 
     @classmethod
     def mockup(cls):
-        cls.session = cls.create_session()
+        cls.session = cls.create_session(expire_on_commit=True)
+        message1 = Message(body='This is message 1', mime_type='text/plain')
+        message2 = Message(body='This is message 2', mime_type='text/plain')
         user1 = User(
             email='user1@example.com',
             title='user1',
             access_token='access token1',
-            reference_id=2
+            reference_id=2,
+            messages=[message1, message2]
         )
         user2 = User(
             email='user2@example.com',
@@ -21,18 +24,10 @@ class TestDeleteMessage(AutoDocumentationBDDTest):
             access_token='access token2',
             reference_id=3
         )
-        room = Room(title='room', type='room')
+        room = Room(title='room', type='room', messages=[message1, message2])
         room.members.append(user1)
         cls.session.add(user2)
         cls.session.add(room)
-        cls.session.flush()
-        message1 = Message(body='This is message 1', mime_type='text/plain')
-        message2 = Message(body='This is message 2', mime_type='text/plain')
-        message1.target_id = room.id
-        message1.sender_id = user1.id
-        message2.target_id = room.id
-        message2.sender_id = user1.id
-        cls.session.add_all([message1, message2])
         cls.session.commit()
 
     def test_delete_the_message(self):
@@ -44,8 +39,14 @@ class TestDeleteMessage(AutoDocumentationBDDTest):
             'DELETE'
         ):
             assert status == 200
-            assert response.json['body'] == 'This is message 1'
-            assert len(self.session.query(Message).all()) == 1
+            assert response.json['body'] == 'This message is deleted'
+            assert len(self.session.query(Message).all()) == 2
+            message1 = self.session.query(Message).filter(Message.id==1).one()
+            assert message1.body == 'This message is deleted'
+            assert message1.is_deleted == True
+
+            when('Delete the same message')
+            assert status == '616 Message Already Deleted'
 
             when('The message not exists', url_parameters=Update(id=3))
             assert status == '614 Message Not Found'
