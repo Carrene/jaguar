222ed7425c09a07b7961c6615bd61bda8113c0be
Farzaneh Kamalou <f70.kamalou@gmail.com>
2019-01-16 15:11:31 +0330

    created god and user3 members

--- jaguar/mockup.py
+++ jaguar/mockup.py
@@ -4,23 +4,39 @@ from .models import Member, Room, Direct
 
 
 def insert():
+    god = Member(
+        id=1,
+        email='god@example.com',
+        title='GOD',
+        access_token='access token',
+        reference_id=1
+    )
     user1 = Member(
+        id=2,
         email='user1@example.com',
-        title='user1',
+        title='user_1',
         access_token='access token1',
         reference_id=2
     )
     user2 = Member(
+        id=3,
         email='user2@example.com',
-        title='user2',
+        title='user_2',
         access_token='access token2',
         reference_id=3
+    )
+    user3 = Member(
+        id=4,
+        email='user3@example.com',
+        title='user_3',
+        access_token='access token3',
+        reference_id=4
     )
     room = Room(
         title='example',
         members=[user1]
     )
-    direct = Direct(members=[user1, user2])
+    direct = Direct(members=[god, user1, user2, user3])
 
     DBSession.add(room)
     DBSession.add(direct)
