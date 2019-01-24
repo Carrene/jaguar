4bc5d0277c2a2cff608b3c6aaf7f2a10571d8bf3
shayan <shayan>
2019-01-24 16:41:16 +0330

    Add the query

--- jaguar/mockup.py
+++ jaguar/mockup.py
@@ -1,6 +1,8 @@
+from sqlalchemy import and_
+from sqlalchemy.orm import aliased
 from restfulpy.orm import DBSession
 
-from .models import Member, Room, Direct
+from .models import Member, Room, Direct, TargetMember, Target
 
 
 def insert():
@@ -34,11 +36,33 @@ def insert():
     )
     room = Room(
         title='example',
-        members=[user1]
+        members=[god, user1, user2, user3]
     )
-    direct = Direct(members=[god, user1, user2, user3])
+    direct1 = Direct(members=[god, user1])
+    direct2 = Direct(members=[god, user2])
 
     DBSession.add(room)
-    DBSession.add(direct)
+    DBSession.add(direct1)
+    DBSession.add(direct2)
     DBSession.commit()
+
+    tm1 = aliased(TargetMember)
+    tm2 = aliased(TargetMember)
+    t = aliased(Target)
+    target = DBSession.query(tm1) \
+        .join(tm2, tm1.target_id == tm2.target_id) \
+        .filter(tm1.member_id == 1) \
+        .filter(tm2.member_id == 2) \
+        .join(t, t.id == tm1.target_id) \
+        .filter(t.type == 'direct') \
+        .all()
+
+    for i in target:
+        print(i.target_id)
+
+
+
+# select t.id from target_member tm1 join target_member tm2 on tm1.target_id
+# = tm2.target_id AND tm1.member_id != tm2.member_id AND tm1.member_id = 1 AND tm2.member_id = 2 join target t on t.id = tm1.target_id and t.type = 'direct';
+
 
