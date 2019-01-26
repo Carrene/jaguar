975d38de4ab30151e4baea6ba6f9102269788054
shayan <shayan>
2019-01-26 13:13:17 +0330

    Add tests of mentioning member, closes #249

--- jaguar/controllers/mention.py
+++ jaguar/controllers/mention.py
@@ -1,10 +1,11 @@
-from nanohttp import json, context, settings
+from sqlalchemy.orm import aliased
+from nanohttp import json, context, settings, HTTPNotFound
 from restfulpy.authorization import authorize
 from restfulpy.controllers import ModelRestController
 from restfulpy.orm import DBSession, commit
 
 from ..messaging import queues
-from ..models import Mention, Member, Direct
+from ..models import Mention, Member, Direct, TargetMember, Target
 from ..validators import mention_validator
 
 
@@ -18,19 +19,43 @@ class MentionController(ModelRestController):
     @authorize
     @mention_validator
     @json
-    @Mention.expose
     @commit
-    def mention(self, target_id):
+    def mention(self):
         mention = Mention()
         mention.body = context.form.get('body')
+
+        if not (self.target or self.member):
+            raise HTTPNotFound()
+
         if self.target:
-            mention.target_id = int(target_id)
+            mention.target_id = self.target.id
+            mention.sender_id = Member.current().id
         else:
-            direct = DBSession.query(
+            mentioner = Member.current()
+            mentioned = self.member
 
-        mention.sender_id = Member.current().id
+            t = aliased(Target)
+            tm1 = aliased(TargetMember)
+            tm2 = aliased(TargetMember)
+
+            target_member = DBSession.query(tm1) \
+                .join(tm2, tm1.target_id == tm2.target_id) \
+                .filter(tm1.member_id == mentioned.id) \
+                .filter(tm2.member_id == mentioner.id) \
+                .join(t, t.id == tm1.target_id) \
+                .filter(t.type == 'direct') \
+                .one_or_none()
+
+            mention.sender_id = mentioner.id
+            if target_member is None:
+                direct = Direct(members=[mentioner, mentioned])
+                DBSession.add(direct)
+                DBSession.flush()
+                mention.target_id = direct.id
+            else:
+                mention.target_id = target_member.target_id
+
         DBSession.add(mention)
-
         queues.push(settings.messaging.workers_queue, mention.to_dict())
 
         return mention
--- jaguar/mockup.py
+++ jaguar/mockup.py
@@ -1,6 +1,5 @@
-from sqlalchemy import and_
-from sqlalchemy.orm import aliased
 from restfulpy.orm import DBSession
+from sqlalchemy.orm import aliased
 
 from .models import Member, Room, Direct, TargetMember, Target
 
@@ -45,24 +44,4 @@ def insert():
     DBSession.add(direct1)
     DBSession.add(direct2)
     DBSession.commit()
-
-    tm1 = aliased(TargetMember)
-    tm2 = aliased(TargetMember)
-    t = aliased(Target)
-    target = DBSession.query(tm1) \
-        .join(tm2, tm1.target_id == tm2.target_id) \
-        .filter(tm1.member_id == 1) \
-        .filter(tm2.member_id == 2) \
-        .join(t, t.id == tm1.target_id) \
-        .filter(t.type == 'direct') \
-        .all()
-
-    for i in target:
-        print(i.target_id)
-
-
-
-# select t.id from target_member tm1 join target_member tm2 on tm1.target_id
-# = tm2.target_id AND tm1.member_id != tm2.member_id AND tm1.member_id = 1 AND tm2.member_id = 2 join target t on t.id = tm1.target_id and t.type = 'direct';
-
 
