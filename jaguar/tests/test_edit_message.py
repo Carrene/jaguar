1b6a805944c112acd5b4b3a8a10b919b866cc64a
mohammad <mohammadsheikhian70@gmail.com>
2019-01-31 10:47:35 +0330

    Fixing a bug on the editing a message with attachment, closes #273, closes #223, closes #169, closes #9

--- jaguar/tests/test_edit_message.py
+++ jaguar/tests/test_edit_message.py
@@ -1,71 +1,84 @@
-from bddrest.authoring import when, status, response, Update
+import io
+from os.path import abspath, join, dirname
 
-from jaguar.models import Member, Room, Message
+from bddrest.authoring import when, status, response, Update
+from sqlalchemy_media import StoreManager
+
+from jaguar.models import Member, Message, Room
 from jaguar.tests.helpers import AutoDocumentationBDDTest, cas_mockup_server
+
+
+THIS_DIR = abspath(join(dirname(__file__)))
+IMAGE_PATH = join(THIS_DIR, 'stuff', '150x150.png')
 
 
 class TestEditMessage(AutoDocumentationBDDTest):
 
     @classmethod
     def mockup(cls):
-        cls.session = cls.create_session()
-        message1 = Message(
-            body='This is message 1',
-            mimetype='text/plain',
-        )
-        message2 = Message(
-            body='This is message 2',
-            mimetype='text/plain',
-        )
-        cls.message3 = Message(
-            body='This is message 3',
-            mimetype='text/plain',
-        )
-        user1 = Member(
-            email='user1@example.com',
-            title='user1',
-            access_token='access token1',
-            reference_id=2,
-            messages=[message1, message2, cls.message3]
-        )
-        user2 = Member(
-            email='user2@example.com',
-            title='user2',
-            access_token='access token2',
-            reference_id=3
-        )
-        room = Room(
-            title='room',
-            type='room',
-            members=[user1],
-            messages=[message1, message2, cls.message3]
-        )
-        cls.session.add(user2)
-        cls.session.add(room)
-        cls.message3.soft_delete()
-        cls.session.commit()
+        session = cls.create_session()
+        with StoreManager(session):
+            with open(IMAGE_PATH, 'rb') as f:
+                cls.message1 = Message(
+                    body='This is message 1',
+                    mimetype='text/plain',
+                    attachment=io.BytesIO(f.read()),
+                )
+                message2 = Message(
+                    body='This is message 2',
+                    mimetype='text/plain',
+                )
+                cls.message3 = Message(
+                    body='This is message 3',
+                    mimetype='text/plain',
+                )
+                cls.user1 = Member(
+                    email='user1@example.com',
+                    title='user1',
+                    access_token='access token1',
+                    reference_id=2,
+                    messages=[cls.message1, message2, cls.message3]
+                )
+                cls.user2 = Member(
+                    email='user2@example.com',
+                    title='user2',
+                    access_token='access token2',
+                    reference_id=3
+                )
+                session.add(cls.user2)
+
+                room = Room(
+                    title='room',
+                    type='room',
+                    members=[cls.user1],
+                    messages=[cls.message1, message2, cls.message3]
+                )
+                session.add(room)
+                cls.message3.soft_delete()
+                session.commit()
 
     def test_edit_the_message(self):
-        self.login('user1@example.com')
+        self.login(self.user1.email)
 
         with cas_mockup_server(), self.given(
-            'Try to edit a message',
-            '/apiv1/messages/id:1',
-            'EDIT',
+            f'Try to edit a message',
+            f'/apiv1/messages/id:{self.message1.id}',
+            f'EDIT',
             form=dict(body='Message 1 is edited')
         ):
             assert status == 200
             assert response.json['body'] == 'Message 1 is edited'
             assert response.json['id'] == 1
+            assert response.json['attachment'] is not None
 
-            when('The message not exists', url_parameters=Update(id=4))
-            assert status == '614 Message Not Found'
+            when('The message not exists', url_parameters=Update(id=0))
+            assert status == 404
 
             when(
                 'Trying to pass using invalid message id',
-                url_parameters=Update(id='Invalid')
+                url_parameters=Update(id='not-integer')
             )
-            assert status == '707 Invalid MessageId'
+            assert status == 404
 
             when(
                 'Try to send long text',
@@ -80,7 +93,7 @@ class TestEditMessage(AutoDocumentationBDDTest):
             assert status == '616 Message Already Deleted'
 
             self.logout()
-            self.login('user2@example.com')
+            self.login(self.user2.email)
 
             when(
                 'Not allowed to edit the message',
