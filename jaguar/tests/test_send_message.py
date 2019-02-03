8af89fe025172954cca882197986b2f2ca4c76d5
Mohammad Sheikhian <39209592+mohammadsheikhian@users.noreply.github.com>
2019-02-03 12:52:21 +0330

    Adding new test case related to auditlog (#280)

--- jaguar/tests/test_send_message.py
+++ jaguar/tests/test_send_message.py
@@ -1,3 +1,4 @@
+import ujson
 from bddrest.authoring import when, Update, status, response, Remove
 
 from jaguar.models import Member, Room
@@ -9,12 +10,13 @@ class TestSendMessage(AutoDocumentationBDDTest):
     @classmethod
     def mockup(cls):
         session = cls.create_session()
-        user1 = Member(
+        cls.user1 = Member(
             email='user1@example.com',
             title='user1',
             access_token='access token1',
             reference_id=2
         )
+
         user2 = Member(
             email='user2@example.com',
             title='user2',
@@ -22,31 +24,26 @@ class TestSendMessage(AutoDocumentationBDDTest):
             reference_id=3
         )
         session.add(user2)
-        room = Room(
+
+        cls.room = Room(
             title='example',
-            members=[user1]
+            members=[cls.user1]
         )
-        session.add(room)
+        session.add(cls.room)
         session.commit()
 
     def test_send_message_to_target(self):
-        self.login('user1@example.com')
+        self.login(self.user1.email)
 
         with cas_mockup_server(), self.given(
-            'Send a message to a target',
-            '/apiv1/targets/id:1/messages',
-            'SEND',
+            f'Send a message to a target',
+            f'/apiv1/targets/id:{self.room.id}/messages',
+            f'SEND',
             form=dict(body='hello world!', mimetype='text/plain')
         ):
             assert status == 200
             assert response.json['body'] == 'hello world!'
             assert response.json['isMine'] is True
-
-            when(
-                'Trying to pass with application/x-auditlog minetype',
-                form=Update(minetype='application/x-auditlog'),
-            )
-            assert status == 200
 
             when('Invalid target id', url_parameters=Update(id='Invalid'))
             assert status == '706 Invalid Target Id'
@@ -71,4 +68,19 @@ class TestSendMessage(AutoDocumentationBDDTest):
 
             when('Try to pass an unauthorized request', authorization=None)
             assert status == 401
+
+    def test_send_message_as_auditlog(self):
+        self.login(self.user1.email)
+        body = dict(action='Create', attribute='a', new=1, old=2)
+        mimetype = 'application/x-auditlog'
+
+        with cas_mockup_server(), self.given(
+            None,
+            f'/apiv1/targets/id:{self.room.id}/messages',
+            f'SEND',
+            json=dict(body=ujson.dumps(body), mimetype=mimetype)
+        ):
+            assert status == 200
+            assert response.json['body'] == body
+            assert response.json['mimetype'] == mimetype
 
