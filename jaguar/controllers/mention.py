009b5caf677cc62dc852028bcd4a04fceb66a793
shayan <shayan>
2019-02-04 12:02:40 +0330

    Fix a bug on mentioning

--- jaguar/controllers/mention.py
+++ jaguar/controllers/mention.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import aliased
-from nanohttp import json, context, settings, HTTPNotFound
+from nanohttp import json, context, settings, HTTPNotFound, HTTPStatus
 from restfulpy.authorization import authorize
 from restfulpy.controllers import ModelRestController
 from restfulpy.orm import DBSession, commit
@@ -34,24 +34,27 @@ class MentionController(ModelRestController):
             mentioner = Member.current()
             mentioned = self.member
 
+            if mentioner.id == mentioned.id:
+                raise HTTPStatus('620 Can Not Mention Yourself')
+
             aliased_target = aliased(Target)
             aliased_target_member1 = aliased(TargetMember)
             aliased_target_member2 = aliased(TargetMember)
 
             target_member = DBSession.query(aliased_target_member1) \
-                .join(
+                .outerjoin(
                     aliased_target_member2,
                     aliased_target_member2.target_id == \
                     aliased_target_member1.target_id
                 ) \
-                .filter(aliased_target_member1.member_id == mentioned.id) \
                 .filter(aliased_target_member2.member_id == mentioner.id) \
+                .filter(aliased_target_member1.member_id == mentioned.id) \
                 .join(
                     aliased_target,
                     aliased_target.id == aliased_target_member1.target_id
                 ) \
                 .filter(aliased_target.type == 'direct') \
-                .one_or_none()
+                .all()
 
             mention.sender_id = mentioner.id
             if target_member is None:
