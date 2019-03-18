6cc2b4fcb7bd75a37bff91a75f352e700a7b4789
ArashFatahzade <fatahzade@gmail.com>
2019-03-18 15:53:25 +0330

    v0.15.3a3

--- jaguar/__init__.py
+++ jaguar/__init__.py
@@ -11,7 +11,7 @@ from .cli import EmailLauncher, MemberLauncher, TargetLauncher, \
     TokenLauncher, WebsocketLauncher, RouterLauncher
 
 
-__version__ = '0.15.2a3'
+__version__ = '0.15.3a3'
 
 
 class Jaguar(Application):
