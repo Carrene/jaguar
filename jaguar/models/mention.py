0b4b5b06493955dd495903d0d8983dd0319f0999
shayan <shayan.rokrok@gmail.com>
2019-04-12 18:09:03 +0430

    Add origin_target_id field to mention model

--- jaguar/models/mention.py
+++ jaguar/models/mention.py
@@ -1,7 +1,22 @@
+from sqlalchemy import Integer
+from restfulpy.orm import Field
+
 from .envelop import Envelop
 
 
 class Mention(Envelop):
 
     __mapper_args__ = {'polymorphic_identity': 'mention'}
+
+    origin_target_id = Field(
+        Integer,
+        python_type=int,
+        minimum=1,
+        required=True,
+        not_none=True,
+        watermark='Loerm Ipsum',
+        example='Loerm Ipsum',
+        message='Loerm Ipsum',
+        label='Origin target',
+    )
 
