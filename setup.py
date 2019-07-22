834c3fd2ce82ff1831f399209c7ce573639e985f
Mohammad Sheikhian <mohammadsheikhian70@gmail.com>
2019-07-22 09:58:30 +0430

    Migrate to restfulpy>3.4.0 (#355)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(join(dirname(__file__), 'jaguar', '__init__.py')) as v_file:
 
 
 dependencies = [
-    'restfulpy >= 3.3.1a5',
+    'restfulpy >= 3.4.0',
     'sqlalchemy_media >= 0.17.1',
     'asyncpg',
 
